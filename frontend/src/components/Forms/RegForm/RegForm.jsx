65b6a11d10c1723c0d43b5a5587efea6902eb81b
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2022-04-04 13:29:52 +0300

    Main page modified

--- frontend/src/components/Forms/RegForm/RegForm.jsx
+++ frontend/src/components/Forms/RegForm/RegForm.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from "./RegForm.module.css";
 import InputOne from "../../IU/Inputs/InputOne/InputOne";
 
 const RegForm = () => {
+
+    const [form, setForm] = useState(true);
+
+    const isForm = () =>{
+        form? setForm(false): setForm(true)
+    }
+
     return (
+        form?
         <form className={classes.regForm}>
-            <div className={classes.link}>
+            <div className={classes.link} onClick={isForm}>
                 Войти
             </div>
             <div className={classes.titel}>
@@ -24,6 +32,27 @@ const RegForm = () => {
                 </button>
             </div>
         </form>
+            :
+        <form className={classes.regForm}>
+            <div className={classes.link} onClick={isForm}>
+                Регистрация
+            </div>
+            <div className={classes.titel}>
+                Войти
+            </div>
+            <div className={classes.margen}><InputOne placeholder="Имя (никнейм)"/></div>
+
+            <InputOne placeholder="Пароль"/>
+            <div className={classes.razdelitel}></div>
+            <InputOne disabled style={{opacity:0}} placeholder="Повторите пароль"/>
+
+            <div className={classes.centr}>
+                <button className={classes.button} >
+                    Отправить
+                </button>
+            </div>
+        </form>
+
     );
 };
 
