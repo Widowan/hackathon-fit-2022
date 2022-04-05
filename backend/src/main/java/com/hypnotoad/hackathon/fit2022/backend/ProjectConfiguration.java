b78582da57bb17c8f079dc70ed82e210c9c2db12
Widowan <Widowan@protonmail.com>
2022-04-05 23:49:51 +0300

    Loose CORS policy

--- backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/ProjectConfiguration.java
+++ backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/ProjectConfiguration.java
@@ -1,9 +1,24 @@
 package com.hypnotoad.hackathon.fit2022.backend;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @PropertySource("classpath:datasourceSecrets.properties")
 public class ProjectConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*")
+                        .allowedOrigins("*")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
