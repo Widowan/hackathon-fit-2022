edf14baa3ab4dfd8c29f7848c939b4488d77b4d2
Kedr123 <48621337+Kedr123@users.noreply.github.com>
2022-04-06 15:04:33 +0300

    Added implementation of registration and authorization

--- frontend/src/API/Api.js
+++ frontend/src/API/Api.js
@@ -1,10 +1,29 @@
+import axios from "axios"
+import serves from "./Serves.js"
 
-// class Api {
-//  // const Domen = "http://nkrutko.ddns.net/api/"
-//
-//  async function reg(){
-//   // axios.get("")
-//  }
-// }
-//
-// export default new Api();
+
+export default class Api {
+ // const Domen = "http://rutko.ddns.net/api/"
+
+ static async reg(name, password){
+  const response = await axios.get(serves.domen()+"signUp?username="+name+"&password="+password);
+  return response;
+ }
+
+ static async auth(name, password){
+  const response = await axios.get(serves.domen()+"signIn?username="+name+"&password="+password);
+  // console.log(response);
+  return response;
+ }
+
+ static async getMe(){
+  const response = await axios.get(serves.domen()+"getMe?token="+localStorage.getItem('token'));
+  return response;
+ }
+
+ static async exit(){
+  const response = await axios.get(serves.domen()+"signOut?token="+localStorage.getItem('token'));
+  return response;
+ }
+}
+
---
+++ frontend/src/API/Serves.js
@@ -0,0 +1,5 @@
+export default class Serves{
+    static domen(){
+        return "http://krutna.ddns.net/api/";
+    }
+}
