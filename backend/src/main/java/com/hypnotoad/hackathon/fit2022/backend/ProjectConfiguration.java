17548362757a35816674d6b2ae230e3047eeefe2
Widowan <Widowan@protonmail.com>
2022-04-08 15:38:22 +0300

    Disable cors

--- backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/ProjectConfiguration.java
+++ backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/ProjectConfiguration.java
@@ -4,10 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @PropertySource("classpath:datasourceSecrets.properties")
 public class ProjectConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*")
+                        .allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
+
     @Bean
     @Value("${url.url}")
     public String hackathonUrl(String url) {
