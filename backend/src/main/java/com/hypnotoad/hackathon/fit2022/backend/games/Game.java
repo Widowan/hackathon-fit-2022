e637ea9facad1c6e8b8b555262e13fbcdce11c2e
Widowan <Widowan@protonmail.com>
2022-04-13 03:56:52 +0300

    Move Game to Immutables

--- backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/games/Game.java
+++ backend/src/main/java/com/hypnotoad/hackathon/fit2022/backend/games/Game.java
@@ -1,49 +1,17 @@
 package com.hypnotoad.hackathon.fit2022.backend.games;
 
-public class Game {
-    private int id;
-    private String name;
-    private String description;
-    private String rules;
-    private String icon;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
 
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getRules() {
-        return rules;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRules(String rules) {
-        this.rules = rules;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
+@JsonDeserialize(as = ImmutableGame.class)
+@JsonSerialize(as = ImmutableGame.class)
+@Value.Immutable
+@Value.Style(stagedBuilder = true)
+public abstract class Game {
+    abstract int    getId();
+    abstract String getName();
+    abstract String getDescription();
+    abstract String getRules();
+    abstract String getIcon();
 }
